d4be3be42be28b7a52c3565adcb1463b819839eb
Sooyeon Chae <cotndus3@naver.com>
2019-08-28 09:05:11 +0900

    #1 수정

--- SoketProgram/Form1.Designer.cs
+++ SoketProgram/Form1.Designer.cs
@@ -54,7 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 449);
+            this.ClientSize = new System.Drawing.Size(311, 429);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
